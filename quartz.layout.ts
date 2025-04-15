4f039ee4c59c0cb891c016e83accf979a4d486df
Kevin Hosford <kevinhosford@live.com>
2025-04-15 23:19:07 +0100

    Quartz sync: Apr 15, 2025, 11:19 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'kevhosford/the-dapaue-files',
+        // from data-repo-id
+        repoId: 'R_kgDOOaGR5w',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOOaGR584CpIis',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://kevhosford.github.io/the-dapaue-files/",
