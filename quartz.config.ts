32abef740b61556f5df39766a0fcc749010dac2f
Kevin Hosford <kevinhosford@live.com>
2025-04-16 23:12:49 +0100

    Quartz sync: Apr 16, 2025, 11:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "The DAPAUE Files",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
